5da7f7bd9a7584983345b263b7714e18f13ce6ae
bcumming <bcumming@cscs.ch>
2023-12-08 15:42:52 +0100

    finish slides

--- src/uenv.sh
+++ src/uenv.sh
@@ -1,9 +1,10 @@
-uenv start --view default gromacs.squashfs
-gmx
-uenv stop
-
 uenv start gromacs.squashfs
-module use /user-environment/modules
+# access the modules provided by the uenv
+uenv modules use
 module load gromacs
+gmx
+
+# alternatively use an environment view
+uenv view default
 gmx
 uenv stop
